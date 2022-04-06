a22843bca8105092ebe3a7152a08d78fe94fa9a7
Kok <kok@Koks-Mac-mini.local>
2022-04-06 18:04:30 +0800

    changed env.js file

--- public/env.js
+++ public/env.js
@@ -1,4 +1,8 @@
 window.env = {
   "KEY": "22bb05d53521a9b4f4e7c24e470b205f647a935243b55a9336315bdb4a89bf1a",
-  "BACK_URL": "litewallet.dev.cryptomailsvc.io"
+  "FAUCET": "https://faucet.dev.meta1.io/faucet",
+  "GATEWAY": "https://gateway.dev.meta1.io/api/wallet/init/",
+  "MAIA": "wss://maia.dev.meta1.io/ws",
+  "BACK_URL": "litewallet.dev.cryptomailsvc.io",
+  "KEY_PREFIX": "DEV11"
 };
