b73890819cc40fe7cc01bf483121ce7788bba6c1
smartworld-dm <25503283+smartworld-dm@users.noreply.github.com>
2022-10-18 07:20:12 -0700

    fix: validateSignature issue

--- src/API/API.js
+++ src/API/API.js
@@ -307,8 +307,7 @@ export async function checkOldUser(accountName) {
 
 export async function validateSignature(accountName, password) {
     try {
-        const payload = buildSignature(accountName, password, true);
-        console.log(payload);
+        const payload = await buildSignature(accountName, password, true);
         const { data } = await axios.post(
             `${process.env.REACT_APP_BACK_URL_DEV1}/validateSignature`,
             payload
@@ -332,7 +331,7 @@ export async function checkMigrationable(accountName) {
 
 export async function migrate(accountName, password) {
   try {
-      const payload = buildSignature(accountName, password);
+      const payload = await buildSignature(accountName, password);
       const { data } = await axios.post(
           `${process.env.REACT_APP_BACK_URL_DEV1}/migrate`,
           payload
