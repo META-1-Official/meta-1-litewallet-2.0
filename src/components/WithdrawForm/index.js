06ea40ea821275a98d31e573db902659bdbf45f1
smartworld-dm <25503283+smartworld-dm@users.noreply.github.com>
2023-02-13 07:34:52 -0800

    fix: deleting input value issue

--- src/components/WithdrawForm/index.js
+++ src/components/WithdrawForm/index.js
@@ -526,12 +526,12 @@ const WithdrawForm = (props) => {
                           value={selectedFromAmount}
                           type={"number"}
                           onChange={(e) => {
+                            if (Number(e.target.value) <= 0) return;
                             if (
-                              (e.target.value.length < 11 &&
-                                /[-+]?[0-9]*\.?[0-9]*/.test(
-                                  e.target.value
-                                ) &&
-                                Number(e.target.value) >= 0)
+                              (
+                                e.target.value.length < 11 &&
+                                /[-+]?[0-9]*\.?[0-9]*/.test(e.target.value)
+                              )
                               || selectedFromAmount?.length > e.target.value.length
                             ) {
                               setSelectedFromAmount(e.target.value);
