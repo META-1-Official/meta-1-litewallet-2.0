07d3d068115c09e01af78c141b4516bc42997682
antmanstar <antman357357@gmail.com>
2023-04-26 01:06:14 +0800

    replaced webcam lib

--- src/components/LoginScreen/FaceKiForm.js
+++ src/components/LoginScreen/FaceKiForm.js
@@ -1,26 +1,36 @@
 import React, { useState, useRef, useEffect } from "react";
-import Webcam from 'react-webcam';
-import { verify, getUserKycProfile } from "../../API/API";
-import OvalImage from '../../images/oval/oval19.png';
-import MobileOvalImage from '../../images/oval/oval12.png';
+import { verify, getUserKycProfile, livenessCheck } from "../../API/API";
+import OvalImage from '../../images/oval/oval.png';
+import { Camera } from 'react-camera-pro';
+import useWidth from '../../lib/useWidth';
+
 import "./login.css";
 
 export default function FaceKiForm(props) {
   const webcamRef = useRef(null);
   const [faceKISuccess, setFaceKISuccess] = useState(false);
-  const [device, setDevice] = React.useState({});
   const [verifying, setVerifying] = useState(false);
+  const [devices, setDevices] = useState([]);
+  const [activeDeviceId, setActiveDeviceId] = useState('');
+  const [numberOfCameras, setNumberOfCameras] = useState(0);
 
-  const browserstack_test_accounts = ['gem-1', 'test-automation', 'john-doe', 'olive-5', 'marry-14', 'mary-14', 'bond-03', 'rock-64', 'rock-3', 'bond-02', 'antman-kok357', 'user-x01'];
+  const width = useWidth();
+
+  const browserstack_test_accounts = ['gem-1', 'test-automation', 'john-doe', 'olive-5', 'marry-14', 'mary-14', 'bond-03', 'rock-64', 'rock-3', 'antman-kok357', 'bond-02', 'user-x01'];
   const errorCase = {
     "Camera Not Found": "Please check your camera.",
     "Not Matched": "Email and wallet name are not matched.",
     "Verify Failed": "We can not verify you because you never enrolled with your face yet.",
     "No Users": "You never enrolled with your face yet. Please enroll first via signup process.",
     "Spoof Detected": "Spoof detected. Are you trying with your real live face?",
-    "Face not Detected": "Face not detected. Try again by changing position or background.",
+    "Face not Detected": "Try again by changing position or background.",
     "Invalid Email": "Bio-metric verification failed for this email. Please use an email that has been linked to your biometric verification / enrollment.",
-    "Biometic Server Error": "Something went wrong from Biometric server."
+    "Biometic Server Error": "Something went wrong from Biometric server.",
+    noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
+    permissionDenied: 'Permission denied. Please refresh and give camera permission.',
+    switchCamera:
+      'It is not possible to switch camera to different one because there is only one video device accessible.',
+    canvas: 'Canvas is not supported.',
   }
 
   useEffect(() => {
@@ -44,9 +54,11 @@ export default function FaceKiForm(props) {
 
     if (flag) {
       return navigator.mediaDevices
-        .getUserMedia(features)
-        .then((display) => {
-          setDevice(display?.getVideoTracks()[0]?.getSettings());
+        .enumerateDevices(features)
+        .then((devices) => {
+          const videoDevices = devices.filter((i) => i.kind == 'videoinput');
+          setDevices(videoDevices);
+          setActiveDeviceId(videoDevices[0]?.deviceId);
         })
         .finally(() => {
           return true;
@@ -80,32 +92,12 @@ export default function FaceKiForm(props) {
   }
 
   const checkAndVerify = async (photoIndex) => {
-    const { privKey, email, accountName } = props;
-    if (!email || !privKey || !accountName) return;
+    const { privKey, email } = props;
+    if (!email || !privKey) return;
 
     setVerifying(true);
 
-    if (photoIndex === 0) {
-      const response_user = await getUserKycProfile(email);
-
-      if (!response_user?.member1Name) {
-        alert(errorCase['Not Matched']);
-        setVerifying(false);
-        return;
-      }
-      else {
-        const walletArry = response_user.member1Name.split(',');
-
-        if (!walletArry.includes(accountName)) {
-          alert(errorCase['Not Matched']);
-          setVerifying(false);
-          return;
-        }
-      };
-    }
-
-    var sizeForSreenShot = isMobile() && device.width ? { width: device.width, height: device.height } : { width: 1280, height: 720 };
-    const imageSrc = webcamRef.current.getScreenshot(sizeForSreenShot);
+    const imageSrc = webcamRef.current.takePhoto();
 
     if (!imageSrc) {
       alert(errorCase['Camera Not Found']);
@@ -114,30 +106,64 @@ export default function FaceKiForm(props) {
     }
 
     const file = await dataURL2File(imageSrc, 'a.jpg');
-    const response = await verify(file);
+    const response = await livenessCheck(file);
 
-    if (!response) {
+    if (!response || !response.data) {
       alert(errorCase['Biometic Server Error']);
       setVerifying(false);
       return;
     }
 
-    if (response.status !== 'Verify OK' && photoIndex === 5) {
-      alert(errorCase[response.status]);
+    if (response.data.liveness !== 'Genuine' && photoIndex === 5) {
+      alert(errorCase['Face not Detected']);
       setVerifying(false);
-    } else if (response.status === 'Verify OK') {
-      const nameArry = response.name.split(',');
+    } else if (response.data.liveness === 'Genuine') {
+      await videoVerify(file);
+    } else {
+      await checkAndVerify(photoIndex + 1);
+    }
+  }
+
+  const videoVerify = async (file) => {
+    const { email, accountName } = props;
+
+    const response_user = await getUserKycProfile(email);
+
+    if (!response_user?.member1Name) {
+      alert(errorCase['Not Matched']);
+      return;
+    }
+    else {
+      const walletArry = response_user.member1Name.split(',');
+
+      if (!walletArry.includes(accountName)) {
+        alert(errorCase['Not Matched']);
+        return;
+      }
+    };
+
+    const response_verify = await verify(file);
+    if (response_verify.status === 'Verify OK') {
+      const nameArry = response_verify.name.split(',');
 
       if (nameArry.includes(email)) {
         setFaceKISuccess(true);
       } else {
         alert(errorCase['Invalid Email']);
-        setVerifying(false);
       }
-    } else {
-      await checkAndVerify(photoIndex + 1);
+    } else if (response_verify.status === 'Verify Failed') {
+      alert(errorCase['Verify Failed']);
+    } else if (response_verify.status === 'No Users') {
+      alert(errorCase['No Users']);
+    }
+    else {
+      alert('Please try again.');
     }
   }
+
+
+  const camWidth = width > 576 ? 600 : width - 30;
+  const camHeight = camWidth / 1.07;
 
   return (
     <div style={{ height: "110%" }} className={"totalSumBlock"}>
@@ -148,9 +174,9 @@ export default function FaceKiForm(props) {
               <h6 style={{ fontSize: '24px' }}>Authenticate Your Face</h6>
               <p className='header_ptag'>To log into your wallet, please complete biometric authentication.</p>
             </div>
-            <div className='child-div' style={{ borderRadius: '5px' }}>
+            <div className='child-div' style={{ width: camWidth, height: camHeight }}>
               <div style={{ width: '100%', display: 'flex', height: '30px', zIndex: '5' }}>
-                <div className="position-head color-black">{!isMobile() ? 'Position your face in the oval' : ''}</div>
+                <div className="position-head color-black">Position your face in the oval</div>
                 <button
                   className='btn_x'
                   onClick={() => {
@@ -159,12 +185,18 @@ export default function FaceKiForm(props) {
                     });
                   }}>X</button>
               </div>
-              <img src={isMobile() ? MobileOvalImage : OvalImage} alt='oval-image' className='oval-image' />
-              <Webcam
-                audio={false}
+              <img src={OvalImage} alt='oval-image' className='oval-image' />
+              <Camera
                 ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                mirrored
+                aspectRatio="cover"
+                numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
+                videoSourceDeviceId={activeDeviceId}
+                errorMessages={{
+                  noCameraAccessible: errorCase.noCameraAccessible,
+                  permissionDenied: errorCase.permissionDenied,
+                  switchCamera: errorCase.switchCamera,
+                  canvas: errorCase.canvas,
+                }}
               />
               <div className='btn-div'>
                 <p className={`span-class color-black margin-bottom-zero ${isMobile() ? 'verify-text-font-size' : ''}`}>{faceKISuccess === false ? 'Press verify to complete authentication and log in' : 'Verification Successful!'}</p>
@@ -174,12 +206,23 @@ export default function FaceKiForm(props) {
                 <span className={`span-class color-black margin-bottom-zero ${isMobile() ? 'camera-text-font-size' : ''}`}>
                   Verifying will take 10 seconds as maximum.
                 </span>
-                <div className="btn-grp" style={{ "marginTop": '5px' }}>
+                <div className="btn-grp">
                   <button className='btn-1' onClick={() => checkAndVerify(0)} disabled={verifying}>{verifying ? "Verifying..." : "Verify"}</button>
                 </div>
               </div>
             </div>
           </div>
+          <select
+            onChange={(event) => {
+              setActiveDeviceId(event.target.value);
+            }}
+          >
+            {devices.map((d) => (
+              <option key={d.deviceId} value={d.deviceId}>
+                {d.label}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
     </div>
