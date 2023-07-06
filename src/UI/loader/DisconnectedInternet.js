664ae847b0cf30f13857dc8146f6207840f84c60
kumarsunnykbs <kumarsunny9@gmail.com>
2023-07-06 15:17:25 +0530

    Litewallet_internet_disconnection_issue_dev2 fixed

---
+++ src/UI/loader/DisconnectedInternet.js
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from "react";
+// import './disconnectedinternet.css';
+
+const DisconnectedInternet = (props) => {
+
+    return (
+        <div>
+            Please check your internet connection.
+        </div>
+    )
+}
+
+export default DisconnectedInternet;
