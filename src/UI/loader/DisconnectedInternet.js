3e254103f3f9779a344672ebe0e8d877a3ee9ff3
kumarsunnykbs <kumarsunny9@gmail.com>
2023-07-07 15:36:51 +0530

    lite_wallet_internet_disconnection_layout_dev2 fixed

--- src/UI/loader/DisconnectedInternet.js
+++ src/UI/loader/DisconnectedInternet.js
@@ -1,11 +1,32 @@
 import React, { useState, useRef, useEffect } from "react";
-// import './disconnectedinternet.css';
+import './disconnectedinternet.css';
+
+import AppStore from "../../images/app-store.png";
+import GooglePlay from "../../images/google-play.png";
+import OfflieIcon from "../../images/offline.png";
 
 const DisconnectedInternet = (props) => {
+    const QRCodeScanCard = (props) =>
+        <div className="qr-code-card">
+            <img src={props.code}></img>
+            <span className="qr-code-text">{props.text}</span>
+        </div>
 
     return (
-        <div>
-            Please check your internet connection.
+        <div className="di-wrapper">
+            <div className="left-side">
+                <div className="text1">Internet Connection</div>
+                <div className="text2">Error</div>
+                <div className="text3">Unable to connect to the internet</div>
+                <div className="text4">Second option scan the QR code and download the app</div>
+                <div className="qr-code-wrapper">
+                    <QRCodeScanCard code={AppStore} text="App Store" />
+                    <QRCodeScanCard code={GooglePlay} text="Google Play" />
+                </div>
+            </div>
+            <div className="right-side">
+                <img src={OfflieIcon}></img>
+            </div>
         </div>
     )
 }
