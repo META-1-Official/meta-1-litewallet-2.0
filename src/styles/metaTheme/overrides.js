7dd86d6d10acd93a39cca241c1265d3f33007018
kumarsunnykbs <kumarsunny9@gmail.com>
2023-08-08 17:22:00 +0530

    litewallet_dark_theme_dev2 fixed

---
+++ src/styles/metaTheme/overrides.js
@@ -0,0 +1,12 @@
+import { breakpoints, palette, pxToRem } from './defaultTheme';
+
+/**
+ * Component overrides
+ *
+ * Now that we have our defaultTheme, we can use
+ * its extracted properties to create our overrides,
+ * and then include it in our overall theme
+ */
+const overrides = Object.freeze({});
+
+export default overrides;
