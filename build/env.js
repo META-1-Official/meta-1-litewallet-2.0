b4ff3fec47f29f8486aa60013a34250571798b04
Aleksandr Ufimtsev <fimak@bk.ru>
2023-09-22 17:38:03 +0600

    ignore build folder

--- build/env.js
+++
@@ -1,8 +0,0 @@
-window.env = {
-  "KEY": "98307131203e0388192fac49a696025d327adff04359f503f39cb54528706414",
-  "FAUCET": "https://faucet.meta1.io/faucet",
-  "GATEWAY": "https://gateway.api.meta1.io/api/wallet/init/",
-  "MAIA": "wss://maia.meta1.io/ws",
-  "BACK_URL": "litewallet.cryptomailsvc.io",
-  "KEY_PREFIX": "META1"
-};
