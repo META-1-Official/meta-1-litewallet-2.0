1f9585da2138ba359de8ff625f4d1615b6da5b6e
kumarsunnykbs <kumarsunny9@gmail.com>
2023-10-09 15:25:53 +0530

    litewallet_ui_dark_theme_650_dev2 fixed

--- src/components/LeftPanelAdapt/LeftPanelAdapt.js
+++ src/components/LeftPanelAdapt/LeftPanelAdapt.js
@@ -96,7 +96,6 @@ const LeftPanelAdapt = (props) => {
             style={{ marginTop: "1.4rem", marginRight: "1rem" }}
           >
             <i
-              style={{ color: "#240000" }}
               className="fas fa-chevron-right event"
             />
           </div>
