1e72d2be1e4cf23963aaf89a30696c7fea540ca0
smartworld-dm <25503283+smartworld-dm@users.noreply.github.com>
2023-10-10 07:46:12 -0700

    fix: migrate() api response on error case

--- src/components/LoginScreen/index.jsx
+++ src/components/LoginScreen/index.jsx
@@ -61,7 +61,7 @@ export default function LoginScreen(props) {
   const dispatch = useDispatch();
   const [authModalOpen, setAuthModalOpen] = useState(false);
 
-  const browserstack_test_accounts = ['gem-1', 'test-automation', 'john-doe', 'olive-5', 'marry-14', 'mary-14', 'bond-03', 'rock-64', 'rock-3', 'bond-02', 'antman-kok357', 'user-x01'];
+  const browserstack_test_accounts = ['gem-1', 'test-automation', 'john-doe', 'olive-5', 'marry-14', 'mary-14', 'bond-03', 'rock-64', 'rock-3', 'bond-02', 'antman-kok357', 'user-x01', 'jin124'];
 
   useEffect(() => {
     if (signatureErrorState) {
@@ -147,7 +147,7 @@ export default function LoginScreen(props) {
         setOpenModal(true);
         setIsMigrationPasskeyValid(true);
       } else {
-        setMigrationMsg('Something went wrong');
+        setMigrationMsg(response_migrate['message']);
         setOpenModal(true);
       }
     } else {
